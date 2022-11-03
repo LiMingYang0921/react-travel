80b877da5cdb31b2200e61da0f192bb33cbcf710
曾经沧海 <2426502977@qq.com>
2022-11-03 21:55:13 +0800

    产品组件
    产品组件

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react"
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import { Spin } from 'antd'
+import { Spin, Row, Col, DatePicker } from 'antd'
 import styles from './Detail.module.css'
-import { Header, Footer } from '../../components'
-import { log } from "console"
+import { Header, Footer, ProductIntro } from '../../components'
 
+const { RangePicker } = DatePicker;
 
 export const Detail: React.FC = () => {
   const { touristRouteId } = useParams()
@@ -14,19 +14,15 @@ export const Detail: React.FC = () => {
   const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
-    console.log(123);
 
     const fetchData = async () => {
       setLoading(true)
       try {
         const { data } = await axios.get(`http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`)
-        console.log('data', data);
 
         setProduct(data)
         setLoading(false)
       } catch (error) {
-        console.log(456);
-
         setError(error instanceof Error ? error.message : 'error')
         setLoading(false)
       }
@@ -43,7 +39,23 @@ export const Detail: React.FC = () => {
     <Header />
     <div className={styles['page-content']}>
       <div className={styles['product-intro-container']}>
-
+        <Row>
+          <Col span={13}>
+            <ProductIntro
+              title={product.title}
+              shortDescription={product.description}
+              price={product.originalPrice}
+              coupons={product.coupons}
+              points={product.points}
+              discount={product.price}
+              rating={product.rating}
+              pictures={product.touristRoutePictures.map((p) => p.url)}
+            />
+          </Col>
+          <Col span={11}>
+            <RangePicker style={{ marginTop: 20 }} />
+          </Col>
+        </Row>
       </div>
       <div className={styles['product-detail-anchor']}></div>
       <div id='feature' className={styles['product-detail-container']}></div>
