1cecfc10b45ac03e42572a80efbaafffdd5ffd03
曾经沧海 <2426502977@qq.com>
2023-03-30 20:40:10 +0800

    重新初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import styles from "./App.module.css"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home, Login, Register, Detail } from './pages'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
   return (
-    <div className={styles.App}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/register' element={<Register />}></Route>
-          <Route path='/detail/:touristRouteId' element={<Detail />}></Route>
-          <Route path='*' element={<h1>页面去火星了</h1>}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div >
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
