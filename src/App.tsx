0926d46e7e0845bf28da2ffdbba51f30c01cfd9d
曾经沧海 <2426502977@qq.com>
2023-05-09 22:17:31 +0800

    axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import styles from './App.module.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home, SignIn, Register, Detail } from './pages'
+import axios from 'axios'
+
+axios.defaults.headers['x-icode'] = '37DE453909E2E209'
 
 function App() {
   return (
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,55 @@
 import React from 'react'
 import styles from './Home.module.css'
 import { Header, Footer, SideMenu, Carousel, ProductCollection } from '../../components'
-import { Row, Col, Typography } from 'antd'
-import { productList1, productList2, productList3 } from './mockups'
+import { Row, Col, Typography, Spin } from 'antd'
 import sideImage from '../../assets/images/sider_2019_12-09.png';
 import sideImage2 from '../../assets/images/sider_2019_02-04.png';
 import sideImage3 from '../../assets/images/sider_2019_02-04-2.png';
+import axios from 'axios';
 
-export class Home extends React.Component {
+interface State {
+  productList: any[],
+  loading: boolean,
+  error: string
+}
+
+export class Home extends React.Component<any, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productList: [],
+      loading: true,
+      error: ''
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get('http://123.56.149.216:8080/api/productCollections')
+      this.setState({
+        productList: data,
+        loading: false,
+        error: ''
+      })
+    } catch (error) {
+      if (error instanceof Error) {
+        this.setState({
+          loading: false,
+          error: error.message
+        })
+      }
+    }
+
+  }
+
   render(): React.ReactNode {
+    const { productList, loading, error } = this.state
+    if (loading) {
+      return <div className={styles['example']}><Spin size="large" /></div>
+    }
+    if (error) {
+      return <p>网络请求错误</p>
+    }
     return <>
       <Header />
       <div className={styles['page-content']}>
@@ -27,7 +68,7 @@ export class Home extends React.Component {
             </Typography.Title>
           }
           sideImage={sideImage}
-          products={productList1}
+          products={productList?.[0]?.touristRoutes}
         />
         <ProductCollection
           title={
@@ -36,7 +77,7 @@ export class Home extends React.Component {
             </Typography.Title>
           }
           sideImage={sideImage2}
-          products={productList2}
+          products={productList?.[1]?.touristRoutes}
         />
         <ProductCollection
           title={
@@ -45,7 +86,7 @@ export class Home extends React.Component {
             </Typography.Title>
           }
           sideImage={sideImage3}
-          products={productList3}
+          products={productList?.[2]?.touristRoutes}
         />
       </div>
       <Footer />
